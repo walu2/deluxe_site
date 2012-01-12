b0a49ff781a242860fd8bf651bf545a624583f22
Piotr Walkowski <walu911@hotmail.com>
2012-01-12 15:26:03 +0100

    finishing before publish

--- index.php
+++ index.php
@@ -29,37 +29,69 @@
                     'transitionOut'	:	'elastic',
                     'speedIn'		:	600, 
                     'speedOut'		:	200, 
-                    'overlayShow'	:	true
+                    'overlayShow'	:	true,
+                    'autoScale' : true
                 });
 	
             });
         </script>
+        <script type="text/javascript">
+            window.___gcfg = {lang: 'pl'};
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/pl_PL/all.js#xfbml=1&appId=296994416996797";
+                fjs.parentNode.insertBefore(js, fjs);
+                var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+                po.src = 'https://apis.google.com/js/plusone.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+      
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-28278052-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+
+        </script>
     </head>
     <body>
+
+        <div id="fb-root"></div>
         <div id="content">
 
             <a class="back" href=""></a>
+
+
             <div id="menuWrapper" class="menuWrapper bg1">
                 <ul class="menu" id="menu">
                     <li class="bg1" style="background-position:0 0;">
                         <a id="bg1" href="#">O Firmie</a>
                         <ul class="sub1" style="background-position:0 0;">
-                            <li><a href="#">Kim jesteśmy?</a></li>
-                            <li><a href="#">Co nas wyróżnia?</a></li>
+                            <li><a href="modal_pages/who_we_are.html" class="fancy">Kim jesteśmy?</a></li>
+                            <li><a href="modal_pages/what_differs.html" class="fancy">Co nas wyróżnia?</a></li>
                         </ul>
                     </li>
                     <li class="bg1" style="background-position:-266px 0px;">
                         <a id="bg2" href="#">Usługi</a>
                         <ul class="sub2" style="background-position:-266px 0;">
-                            <li><a href="#">Nasza oferta</a></li>
-                            <li><a href="#">Zadowoleni klienci</a></li>
+                            <li><a href="modal_pages/our_offert.html" class="fancy">Nasza oferta</a></li>
+                            <li><a href="modal_pages/clients.html" class="fancy">Zadowoleni klienci</a></li>
                         </ul>
                     </li>
                     <li class="last bg1" style="background-position:-532px 0px;">
                         <a id="bg3" href="#">Kontakt</a>
                         <ul class="sub3" style="background-position:-266px 0;">
                             <li><a href="modal_pages/contact.html" class="fancy">Kontakt</a></li>
-                            <li><a href="#">Znajdź nas</a></li>
+                            <li><a href="modal_pages/find_us.html" class="fancy">Znajdź nas</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -67,9 +99,15 @@
 
         </div>
 
+        <div class="footer">Copyright © 2010-<?php echo date("Y"); ?> <a href="mailto:piotr@deluxe-soft.com">Deluxe-Soft</a>. Wszelkie prawa zastrzeżone. 
+        </div>
+        <center>
+            <g:plusone size="small" annotation="inline" width="100px"></g:plusone>
 
-        <br />
-        <div class="footer">Copyright © <?php "2010-{Date("Y")}"; ?> Deluxe-Soft. Wszelkie prawa zastrzeżone. </div>
-        <a id="single_image" href="image_big.jpg"><img src="image_small.jpg" alt=""/></a>
+            <div class="fb-like" data-href="http://www.facebook.com/pages/Deluxe-Soft/210545042304524" data-send="false" data-layout="box_count" data-show-faces="false" data-action="recommend"></div>
+
+        </center>
+
+
     </body>
 </html>
